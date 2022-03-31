3bc2b0a16e19a2ce0c7d8299ea630934dffc3cf9
Francis Shonubi <shonubioladipo@gmail.com>
2022-03-31 21:28:49 +0100

    feat: add responsive designs for header & features
    - added full responsive  designs for all media sizes on both the header
      and the features section.

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -2,36 +2,38 @@ const Features: React.FC = () => {
 
     return (
         /* page container */
-        <div className='container flex flex-col items-center mx-auto'>
+        <div className='container flex flex-col items-center mx-auto mb-12'>
 
             <h3 className='mb-4 text-3xl font-medium text-center'>Features</h3>
-            <p className='w-2/5 m-auto text-xl text-center text-gray-400'>
+            <p className='w-full px-4 lg:w-2/5 m-auto lg:text-xl text-center text-gray-400'>
                 Our aim is to make it quick and easy for you to access your favourite websites.
                 Your bookmarks sync between your devices so you can access them on the go.
             </p>
             {/* feature sections */}
-            <div className='flex justify-center w-3/5 mt-4'>
-                <a href="" className="inline-block px-10 py-3 text-lg text-black border-b-4 border-primary-softRed focus:border-b-4 focus:border-primary-softRed hover:text-primary-softRed">Simple Bookmarking</a>
-                <a href="" className="inline-block px-10 py-3 text-lg text-black focus:border-b-4 focus:border-primary-softRed hover:text-primary-softRed">Speedy Searching</a>
-                <a href="" className="inline-block px-10 py-3 text-lg text-black focus:border-b-4 focus:border-primary-softRed hover:text-primary-softRed">Easy Sharing</a>
-            </div>
-            <hr className='mb-12 w-[36rem] border-t-1 border-slate-200' />
+            <div className='  border-t-[.5px] border-b-[.5px] items-center flex flex-col  w-4/5 border-slate-200 mx-6 mt-4 mb-12 lg:w-fit lg:border-t-0 lg:px-0 lg:flex-row lg:justify-center'>
+                <a href="" className="inline-block py-3 lg:text-lg w-fit text-black border-b-4 border-primary-softRed lg:px-10 focus:border-b-4 focus:border-primary-softRed hover:text-primary-softRed">Simple Bookmarking</a>
+                    <hr className=' w-full border-t-1 border-slate-200 mx-6 lg:hidden' />
+                <a href="" className="inline-block py-3 lg:text-lg w-fit text-black focus:border-b-4 focus:border-primary-softRed lg:px-10 hover:text-primary-softRed">Speedy Searching</a>
+                    <hr className=' w-full border-t-1 border-slate-200 mx-6 lg:hidden' />
+                <a href="" className="inline-block py-3 lg:text-lg w-fit text-black focus:border-b-4 focus:border-primary-softRed lg:px-10 hover:text-primary-softRed">Easy Sharing</a>
+            </div>                                                                                                         
+
 
             {/* features hero */}
-            <div className='flex'>
-                <img src="images/illustration-features-tab-1.svg" alt="hero" className='w-[97rem] mr-24' />
+            <div className='px-3 lg:flex lg:px-6 lg:gap-12 xl:gap-0'>
+                <img src="images/illustration-features-tab-1.svg" alt="hero" className=' w-full md:w-[97rem] lg:w-[27rem] xl:w-[97rem] md:mr-24 xl:mr-24 lg:mr-2' />
 
                 <div className='mt-16'>
-                    <h1 className='text-4xl font-medium'>
+                    <h1 className='text-center mb-2 text-2xl lg:text-4xl lg:text-left font-medium'>
                         Bookmark in one click
                     </h1>
-                    <p className='w-3/4 py-6 text-lg text-gray-400'>
+                    <p className='text-center md:text-center  sm:w-3/4 md:w-full  md:py-6 lg:text-left md:text-lg text-gray-400'>
                         Organize your bookmarks however you like. Our simple drag-and-drop interface
                         gives you complete control over how you manage your favourite sites.
                     </p>
                     {/* cta buttons */}
                     <div>
-                        <a href="#" className="inline-block px-10 py-3 text-sm text-white rounded bg-primary-softBlue hover:text-primary-softRed hover:ring-2 hover:ring-primary-softBlue hover:bg-white hover:text-primary-softBlue ">More Info</a>
+                        <a href="#" className="hidden lg:inline-block px-10 py-3 text-sm text-white rounded bg-primary-softBlue hover:ring-2 hover:ring-primary-softBlue hover:bg-white hover:text-primary-softBlue ">More Info</a>
                     </div>
                 </div>
             </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,33 +4,33 @@ const Header: React.FC = () => {
         /* page container */
         <header className='container px-4 py-8 mx-auto'>
             {/* nav bar */}
-            <div className='flex justify-between item-center'>
+            <nav className='flex justify-between item-center lg:px-4 '>
                 <div>
                     <a href="#" className="flex lg:inline-block py-3 justify-items-start"><img src='images/logo-bookmark.svg' /></a>
                 </div>
                 <div className='hidden lg:block'>
-                    <a href="#" className="inline-block px-10 py-3 text-sm text-black uppercase hover:text-primary-softRed">features</a>
-                    <a href="#" className="inline-block px-10 py-3 text-sm text-black uppercase hover:text-primary-softRed">pricing</a>
-                    <a href="#" className="inline-block px-10 py-3 text-sm text-black uppercase hover:text-primary-softRed">contact</a>
-                    <a href="#" className="inline-block px-10 py-3 text-sm text-white uppercase rounded bg-primary-softRed hover:text-primary-softRed hover:ring-2 hover:ring-red-700 hover:bg-white "> login </a>
+                    <a href="#" className="inline-block xl:px-10 py-3 lg:px-5 text-sm lg:text-xs text-black uppercase hover:text-primary-softRed">features</a>
+                    <a href="#" className="inline-block xl:px-10 py-3 lg:px-5 text-sm lg:text-xs text-black uppercase hover:text-primary-softRed">pricing</a>
+                    <a href="#" className="inline-block xl:px-10 py-3 lg:px-5 text-sm lg:text-xs text-black uppercase hover:text-primary-softRed">contact</a>
+                    <a href="#" className="inline-block xl:px-10 py-3 lg:px-5 text-sm lg:text-xs text-white uppercase rounded bg-primary-softRed hover:text-primary-softRed hover:ring-2 hover:ring-red-700 hover:bg-white "> login </a>
                 </div>
-            </div>
+            </nav>
             {/* hero section */}
-            <div className='flex flex-col lg:flex-row lg:py-20 '>
+            <div className='flex flex-col lg:flex-row lg:py-20 lg:px-4 lg:w-full '>
                 {/* cta section */}
                 <div className='order-last lg:order-1 lg:mt-16'>
-                    <h1 className='text-2xl text-center font-medium lg:text-left lg:text-5xl'>A Simple Bookmark Manager</h1>
-                    <p className='lg:w-3/4 py-6 text-center lg:text-left lg:text-lg text-gray-400'>
+                    <h1 className='text-2xl text-center font-medium lg:text-left xl:text-5xl '>A Simple Bookmark Manager</h1>
+                    <p className='lg:w-3/4 py-6 text-center lg:text-left xl:text-lg text-gray-400'>
                         A clean and simple interface to organize your favourite websites. Open a new
                         browser tab and see your sites load instantly. Try it for free.
                     </p>
                     {/* cta buttons */}
                     <div className="h-11 w-11  md:w-auto md:h-auto sm:flex sm:justify-center sm:w-auto lg:justify-start">
-                        <a href="#" className="whitespace-nowrap text-[.8rem] text-center px-3 py-3 md:inline-block md:px-10 md:py-3 md:text-sm text-white rounded bg-primary-softBlue  hover:ring-2 hover:ring-primary-softBlue hover:bg-white hover:text-primary-softBlue ">Get it on Chrome</a>
-                        <a href="#" className="whitespace-nowrap text-[.8rem] text-center px-3 py-3 mx-5 md:inline-block md:px-9 md:py-3 md:mx-10 md:text-sm text-black rounded shadow-2xl bg-slate-100  hover:ring-2 hover:ring-slate-900 hover:bg-white hover:text-slate-900">Get it on Firefox</a>
+                        <a href="#" className="whitespace-nowrap text-[.8rem] text-center px-3 py-3 md:inline-block md:px-10 md:py-3 md:text-sm lg:px-3 text-white rounded bg-primary-softBlue  hover:ring-2 hover:ring-primary-softBlue hover:bg-white hover:text-primary-softBlue ">Get it on Chrome</a>
+                        <a href="#" className="whitespace-nowrap text-[.8rem] text-center px-3 py-3 mx-5 md:inline-block md:px-9 md:py-3 md:mx-10 md:text-sm lg:px-3 text-black rounded shadow-2xl bg-slate-100  hover:ring-2 hover:ring-slate-900 hover:bg-white hover:text-slate-900">Get it on Firefox</a>
                     </div>
                 </div>
-                <img src="images/illustration-hero.svg" alt="hero" className=' mt-8 w-full lg:w-[92rem] lg:-mr-24 lg:order-last' />
+                <img src="images/illustration-hero.svg" alt="hero" className=' mt-8 w-full md:w-[92rem] lg:w-[28rem] xl:w-[92rem] md:-mr-24 lg:-mr-4 lg:order-last ' />
             </div>
 
         </header>
