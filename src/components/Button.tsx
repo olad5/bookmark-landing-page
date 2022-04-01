83ae923c5142a0d7fa7a567d3671627d6635aad1
Francis Shonubi <shonubioladipo@gmail.com>
2022-04-01 12:49:30 +0100

    feat: add prettier to project
    - ran yarn run format on all files

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,23 +4,38 @@ interface IProps {
   state?: string;
 }
 
-
-const Button: React.FC<IProps> = ({children, color, state='inline-block'}) => {
+const Button: React.FC<IProps> = ({
+  children,
+  color,
+  state = "inline-block",
+}) => {
   // console.log(children);
   // console.log(color);
 
   // - <div className={`${styles.divClasses} ${expanded ? 'block' : 'hidden'}`} >whatever</div>
 
-    return (
-            <a href="#" className={ `px-4 text-[.7rem] sm:text-[.8rem] ${ state } xl:px-10 py-3 lg:px-5 text-sm sm:px-9 lg:text-xs text-white  
-              rounded ${color ==='red' ? 'bg-primary-softRed' : 'bg-primary-softBlue'} 
-              ${color ==='red' ? 'hover:text-primary-softRed hover:ring-red-700' : 'hover:text-primary-softBlue hover:ring-primary-softBlue'} 
-              hover:bg-white hover:ring-2  ` }> 
-              {/* hover:text-primary-softRed  hover:ring-red-700 hover:bg-white hover:ring-2  ` }> */} 
-                {children}
-            </a>
-        )
-
-}
+  return (
+    <a
+      href="#"
+      className={`px-4 text-[.7rem] sm:text-[.8rem] ${state} xl:px-10 py-3 lg:px-5 text-sm sm:px-9 lg:text-xs text-white  
+              rounded ${
+                color === "red" ? "bg-primary-softRed" : "bg-primary-softBlue"
+              } 
+              ${
+                color === "red"
+                  ? "hover:text-primary-softRed hover:ring-red-700"
+                  : "hover:text-primary-softBlue hover:ring-primary-softBlue"
+              } 
+              hover:bg-white hover:ring-2  `}
+    >
+      {/* hover:text-primary-softRed  hover:ring-red-700 hover:bg-white hover:ring-2  ` }> */}
+      {/* hover:text-primary-softRed  hover:ring-red-700 hover:bg-white hover:ring-2  ` }> */}
+            {/* hover:text-primary-softRed  hover:ring-red-700 hover:bg-white hover:ring-2  ` }> */}
+                    {/* hover:text-primary-softRed  hover:ring-red-700 hover:bg-white hover:ring-2  ` }> */}
+      {children}
+      hello there
+    </a>
+  );
+};
 
 export default Button;
--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,48 +1,64 @@
 import Button from "./Button";
 
 const Features: React.FC = () => {
+  return (
+    /* page container */
+    <div className="container flex flex-col items-center mx-auto mb-12">
+      <h3 className="mb-4 text-3xl font-medium text-center">Features</h3>
+      <p className="w-full px-4 lg:w-2/5 m-auto lg:text-xl text-center text-gray-400">
+        Our aim is to make it quick and easy for you to access your favourite
+        websites. Your bookmarks sync between your devices so you can access
+        them on the go.
+      </p>
+      {/* feature sections */}
+      <div className="  border-t-[.5px] border-b-[.5px] items-center flex flex-col  w-4/5 border-slate-200 mx-6 mt-4 mb-12 lg:w-fit lg:border-t-0 lg:px-0 lg:flex-row lg:justify-center">
+        <a
+          href=""
+          className="inline-block py-3 lg:text-lg w-fit text-black border-b-4 border-primary-softRed lg:px-10 focus:border-b-4 focus:border-primary-softRed hover:text-primary-softRed"
+        >
+          Simple Bookmarking
+        </a>
+        <hr className=" w-full border-t-1 border-slate-200 mx-6 lg:hidden" />
+        <a
+          href=""
+          className="inline-block py-3 lg:text-lg w-fit text-black focus:border-b-4 focus:border-primary-softRed lg:px-10 hover:text-primary-softRed"
+        >
+          Speedy Searching
+        </a>
+        <hr className=" w-full border-t-1 border-slate-200 mx-6 lg:hidden" />
+        <a
+          href=""
+          className="inline-block py-3 lg:text-lg w-fit text-black focus:border-b-4 focus:border-primary-softRed lg:px-10 hover:text-primary-softRed"
+        >
+          Easy Sharing
+        </a>
+      </div>
 
-    return (
-        /* page container */
-        <div className='container flex flex-col items-center mx-auto mb-12'>
+      {/* features hero */}
+      <div className="px-3 lg:flex lg:px-6 lg:gap-12 xl:gap-0">
+        <img
+          src="images/illustration-features-tab-1.svg"
+          alt="hero"
+          className=" w-full md:w-[97rem] lg:w-[27rem] xl:w-[97rem] md:mr-24 xl:mr-24 lg:mr-2"
+        />
 
-            <h3 className='mb-4 text-3xl font-medium text-center'>Features</h3>
-            <p className='w-full px-4 lg:w-2/5 m-auto lg:text-xl text-center text-gray-400'>
-                Our aim is to make it quick and easy for you to access your favourite websites.
-                Your bookmarks sync between your devices so you can access them on the go.
-            </p>
-            {/* feature sections */}
-            <div className='  border-t-[.5px] border-b-[.5px] items-center flex flex-col  w-4/5 border-slate-200 mx-6 mt-4 mb-12 lg:w-fit lg:border-t-0 lg:px-0 lg:flex-row lg:justify-center'>
-                <a href="" className="inline-block py-3 lg:text-lg w-fit text-black border-b-4 border-primary-softRed lg:px-10 focus:border-b-4 focus:border-primary-softRed hover:text-primary-softRed">Simple Bookmarking</a>
-                    <hr className=' w-full border-t-1 border-slate-200 mx-6 lg:hidden' />
-                <a href="" className="inline-block py-3 lg:text-lg w-fit text-black focus:border-b-4 focus:border-primary-softRed lg:px-10 hover:text-primary-softRed">Speedy Searching</a>
-                    <hr className=' w-full border-t-1 border-slate-200 mx-6 lg:hidden' />
-                <a href="" className="inline-block py-3 lg:text-lg w-fit text-black focus:border-b-4 focus:border-primary-softRed lg:px-10 hover:text-primary-softRed">Easy Sharing</a>
-            </div>                                                                                                         
-
-
-            {/* features hero */}
-            <div className='px-3 lg:flex lg:px-6 lg:gap-12 xl:gap-0'>
-                <img src="images/illustration-features-tab-1.svg" alt="hero" className=' w-full md:w-[97rem] lg:w-[27rem] xl:w-[97rem] md:mr-24 xl:mr-24 lg:mr-2' />
-
-                <div className='mt-16 sm:flex sm:flex-col  items-center lg:block'>
-                    <h1 className='text-center mb-2 text-2xl lg:text-4xl lg:text-left font-medium'>
-                        Bookmark in one click
-                    </h1>
-                    <p className='text-center sm:m-0  md:text-center  sm:w-3/4 md:w-full  md:py-6 lg:text-left md:text-lg text-gray-400'>
-                        Organize your bookmarks however you like. Our simple drag-and-drop interface
-                        gives you complete control over how you manage your favourite sites.
-                    </p>
-                    {/* cta buttons */}
-                    <div>
-                            <Button state="hidden lg:inline-block">More Info</Button>
-                    </div>
-                </div>
-            </div>
+        <div className="mt-16 sm:flex sm:flex-col  items-center lg:block">
+          <h1 className="text-center mb-2 text-2xl lg:text-4xl lg:text-left font-medium">
+            Bookmark in one click
+          </h1>
+          <p className="text-center sm:m-0  md:text-center  sm:w-3/4 md:w-full  md:py-6 lg:text-left md:text-lg text-gray-400">
+            Organize your bookmarks however you like. Our simple drag-and-drop
+            interface gives you complete control over how you manage your
+            favourite sites.
+          </p>
+          {/* cta buttons */}
+          <div>
+            <Button state="hidden lg:inline-block">More Info</Button>
+          </div>
         </div>
-
-    )
-
-}
+      </div>
+    </div>
+  );
+};
 
 export default Features;
